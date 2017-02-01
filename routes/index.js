a16e2ff5c9fbde4c4e74cca48f5e03dada336486
hector <hvintro@gmail.com>
2017-02-01 22:29:50 +0100

    aleluya

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pg = require('pg');
 const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-const path = require('path');
 
 
 
