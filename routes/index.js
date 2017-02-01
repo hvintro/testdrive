5e7ac23c09deb2b4320c57dbb9d3be3e10030cfa
hector <hvintro@gmail.com>
2017-02-01 19:32:23 +0100

    test2 routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var pg = require('pg');
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/db', function (request, response) {
+router.post('/api/v1/preguntas', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {pregunta: "req.body.text", respuestaa: "false", respuestab:"respuestab",
+  respuestac:"req.body",correcta:0};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query('INSERT INTO preguntas(pregunta, respuestaa, respuestab, respuestac, correcta)'+
+    'values($1, $2, $3, $4,$5)',
+    [data.pregunta, data.respuestaa, data.respuestab, data.respuestac, data.correcta]);
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM preguntas ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+/*router.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM preguntas', function(err, result) {
       done();
@@ -16,6 +50,6 @@ router.get('/db', function (request, response) {
       else
        { response.render('pages/db', {results: result.rows} ); }
     });
-});});
+});});*/
 
 module.exports = router;
