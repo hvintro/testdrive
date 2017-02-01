1c23a02d0a2a23eceacfe87fd6b0e83be0e98c11
hector <hvintro@gmail.com>
2017-02-01 20:58:43 +0100

    test4 routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const pg = require('pg');
 const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const path = require('path');
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   res.sendFile('index.html');
 });
 
 router.post('/api/v1/preguntas', (req, res, next) => {
@@ -40,6 +42,31 @@ router.post('/api/v1/preguntas', (req, res, next) => {
     });
   });
 });
+
+router.get('/api/v1/preguntas', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM preguntas ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
 
 /*router.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
