c4cbf7fdf0c9f2e0d2d398521037bf91a88612d8
hector <hvintro@gmail.com>
2017-02-02 22:06:24 +0100

    las linux

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,13 @@ angular.module('nodeTodo', [])
 .controller('mainController', ($scope, $http) => {
   $scope.formData = {};
   $scope.todoData = {};
+  $scope.formpregunta = {};
+  $scope.formrespuestaa = {};
+  $scope.formrespuestab = {};
+  $scope.formrespuestac = {};
+  $scope.formcorrecta = {};
+  $scope.formexplicacio = {};
+
   // Get all todos
   $http.get('/api/v1/preguntas')
   .success((data) => {
@@ -16,10 +23,19 @@ angular.module('nodeTodo', [])
    console.log("haha" + $scope.formpregunta);
    console.log("haha" + $scope.formcorrecta);
    console.log("haha" + $scope.formexplicacio);
-   var Indata = {'pregunta': $scope.formpregunta, 'respuestaa': $scope.formrespuestaa , 'respuestab': $scope.formrespuestab , 'respuestac': $scope.formrespuestac, 'correcta': $scope.formcorrecta, 'explicacio' :$scope.formexplicacio };
-   $http.post('/api/v1/preguntas', Indata )
+   var Indata = $.param({'pregunta': $scope.formpregunta, 'respuestaa': $scope.formrespuestaa ,
+   'respuestab': $scope.formrespuestab , 'respuestac': $scope.formrespuestac,
+   'correcta': $scope.formcorrecta, 'explicacio' :$scope.formexplicacio });
+
+   var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                    }
+                }
+
+   $http.post('/api/v1/preguntas', Indata, config)
    .success((data) => {
-     $scope.formData = {};
+     $scope.formpregunta = {};
      $scope.todoData = data;
      console.log(data);
    })
@@ -27,17 +43,4 @@ angular.module('nodeTodo', [])
      console.log('Error: ' + error);
    });
  };
-
- $scope.createTodo = () => {
-   $http.post('/api/v1/todos', $scope.formData)
-   .success((data) => {
-     $scope.formData = {};
-     $scope.todoData = data;
-     console.log(data);
-   })
-   .error((error) => {
-     console.log('Error: ' + error);
-   });
- };
-
 });
