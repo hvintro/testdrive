3e23d78a6b9b6f41d7b1bcbce5897da81ef41ca4
hector <hvintro@gmail.com>
2017-02-02 18:31:21 +0100

    add all

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var path = require('path');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/preguntas';
+var connectionString = process.env.DATABASE_URL;
 
 
 
