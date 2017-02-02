f800a844acede506292501a62e437d35cafdf385
hector <hvintro@gmail.com>
2017-02-02 21:42:22 +0100

    hey

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.post('/api/v1/preguntas', (req, res, next) => {
   const results = [];
   // Grab data from http request
   const data = {pregunta: req.body.pregunta, respuestaa: req.body.respuestaa, respuestab:req.body.respuestab,
-  respuestac:req.body.respuestac,correcta:req.body.correcta};
+  respuestac:req.body.respuestac,correcta:req.body.correcta, explicacio:req.body.explicacio};
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, (err, client, done) => {
     // Handle connection errors
@@ -28,9 +28,9 @@ router.post('/api/v1/preguntas', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Insert Data
-    client.query('INSERT INTO preguntas(pregunta, respuestaa, respuestab, respuestac, correcta)'+
-    'values($1, $2, $3, $4,$5)',
-    [data.pregunta, data.respuestaa, data.respuestab, data.respuestac, data.correcta]);
+    client.query('INSERT INTO preguntas(pregunta, respuestaa, respuestab, respuestac, correcta, explicacio)'+
+    'values($1, $2, $3, $4, $5, $6)',
+    [data.pregunta, data.respuestaa, data.respuestab, data.respuestac, data.correcta, data.explicacio]);
     // SQL Query > Select Data
     const query = client.query('SELECT * FROM preguntas ORDER BY id ASC');
     // Stream results back one row at a time
